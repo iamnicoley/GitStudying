958e83eee0c954c5eb28c17f51599647fa1c984c
nicole <contactnicoleyue@icloud.com>
2020-12-15 16:32:44 -0800

    First commit

---
+++ src/test/java/mavenQuickStart/Mavenjava/AppiumTest.java
@@ -0,0 +1,23 @@
+package mavenQuickStart.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+		
+	}
+	
+	
+	
+}
---
+++ src/test/java/mavenQuickStart/Mavenjava/RESTAPITest.java
@@ -0,0 +1,24 @@
+package mavenQuickStart.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void PostJira()
+	{
+		
+		System.out.println("PostJira");
+
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+
+		
+	}
+	
+	
+}
